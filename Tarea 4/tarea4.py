40e998db01b64cdb23166f2b7e51fd9d4e0cdf6b
a24lucasba <a24lucasba@iessanclemente.net>
2025-11-12 11:28:46 +0100

    Tarea4

--- Tarea 4/tarea4.py
+++ Tarea 4/tarea4.py
@@ -2,8 +2,13 @@
 
 from subprocess import run
 from re import findall
+import ipinfo
+import requests
+from time import sleep
 
 run ('clear',shell=True)
+
+handler = ipinfo.getHandler('81a72251e9592e')
 
 patronIP = r'(?:[0-9]{1,3}\.){3}[0-9]{1,3}'
 buscarUser = r'Failed password for (.*)'
@@ -26,10 +31,25 @@ with open(rutaArchivo, 'r') as fr:
                 if not lUsu.split()[2]=='from':
                     usuario = lUsu.split()[2]
                 else:
-                    usuario = 'Blank'
+                    usuario = 'None'
+            
+            datos = handler.getDetails(ip)
+            
+            if hasattr(datos, 'loc') and datos.loc:
+                lat = datos.loc.split(',')[0]
+                lon = datos.loc.split(',')[1]
+            else:
+                lat = 'Desconocido'
+                lon = 'Desconocido'
+            
+            ciudad = getattr(datos, 'city', 'Desconocido'),
+            region = getattr(datos, 'region', 'Desconocido'),
+            pais = getattr(datos, 'country_name', 'Desconocido'),
+            latitud = lat.strip() if lat != 'Desconocido' else 'Desconocido',
+            longitud = lon.strip() if lon != 'Desconocido' else 'Desconocido'
 
             if ip not in dInfoAuthLog:
-                dInfoAuthLog[ip] = {'usuarios': [usuario]}
+                dInfoAuthLog[ip] = {'ciudad' : ciudad,'region':region,'pais':pais,'latitud':latitud,'longitud':longitud,'usuarios': [usuario]}
             else:
                 if usuario not in dInfoAuthLog[ip]['usuarios']:
                     dInfoAuthLog[ip]['usuarios'].append(usuario)
@@ -37,4 +57,4 @@ with open(rutaArchivo, 'r') as fr:
                     pass
 
 for k,v in dInfoAuthLog.items():
-    print (f'Clave: {k}, Valor: {v}')
+    print (f'{k}:{v}')
