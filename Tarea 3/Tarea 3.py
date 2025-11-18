bbbf6affa996da7ae6a5f860408dbeb6a4200179
a24lucasba <a24lucasba.iessanclemente.net>
2025-11-18 18:44:30 +0100

    Tarea 3 Terminada

--- Tarea 3/Tarea 3.py
+++ Tarea 3/Tarea 3.py
@@ -3,12 +3,16 @@
 from subprocess import run
 from requests import get
 from time import sleep
+from datetime import datetime
+from os import path,makedirs
 
 run('clear',shell=True)
 
-# subred = input("Introduce la ip a estudiar: ")
+subred = input("Introduce la ip a estudiar: ")
 
-cping = f"fping -g 192.168.0.0/24 -a -q -e"
+lInfoIps = []
+
+cping = f"fping -g {subred} -a -q -e"
 
 scping = run(cping,shell=True, capture_output=True, text=True).stdout.strip().split('\n')
 
@@ -22,9 +26,21 @@ for l in scping:
     try:
         mac = scneigh[4]
     except:
-        mac = "08:00:27:10:0e:1c"
+        mac = "f0:b6:1e:05:dc:c3" # Aquí iría la mac del equipo
 
     sleep(1.5)
     resp = get(f'https://api.macvendors.com/{mac}')
     fabricante = resp.text
-    print (fabricante)
+
+    dInfoIps = {"ip":ip,"mac":mac,"fabricante":fabricante,"latencia":latencia}
+    lInfoIps.append(dInfoIps)
+
+fecha = datetime.now()
+subredCortada = list(subred)[:-3]
+a = f'hostsRed_{subredCortada}_{fecha.year}_{fecha.month}_{fecha.day}_{fecha.hour}_{fecha.minute}'
+
+with open(f'Tarea 3/{a}','w') as fr:
+        fr.write(f'ip;mac;fabricante;latencia\n')
+        for l in lInfoIps:
+            fr.write(f"{dInfoIps['ip']};{dInfoIps['mac']};{dInfoIps['fabricante']};{dInfoIps['latencia']}")
+    
